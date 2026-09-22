2cedfac7470cb9806a69b8275c5c921d34131590
JaroschPC <jaroschew@gmail.com>
2026-09-22 12:32:36 +0300

    Add Task1

--- PZ1/PZ2/Program.cs
+++ PZ1/PZ2/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Одновимірний масив
+            int[] array1D = new int[3];
+
+            // Двовимірний масив
+            int[,] array2D = new int[3, 3];
+
+            // трьохвимірний масив
+            int[,,] array3D = new int[3, 3, 3];
+
+            // Зубчастий масив (jagged array)
+            int[][] arrayJagged = new int[3][];
+            arrayJagged[0] = new int[1];
+            arrayJagged[1] = new int[2];
+            arrayJagged[2] = new int[3];
+
+            // Заповнення зупчастого масиву випадковими числами
+            Random random = new Random();
+            for (int i = 0; i < arrayJagged.Length; i++)
+            {
+                for (int j = 0; j < arrayJagged[i].Length; j++)
+                {
+                    arrayJagged[i][j] = random.Next(1, 10);
+                }
+            }
+
+            // Виведення зубчастого масиву
+            Console.WriteLine("Зубчастий масив:");
+            for (int i = 0; i < arrayJagged.Length; i++)
+            {
+                for (int j = 0; j < arrayJagged[i].Length; j++)
+                {
+                    Console.Write(arrayJagged[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("======================");
+
+            Task1 task1 = new Task1();
+            task1.Run();
+
         }
     }
 }
---
+++ PZ1/PZ2/Task1.cs
@@ -0,0 +1,69 @@
+﻿
+
+namespace PZ2
+{
+    internal class Task1
+    {
+        public void Run()
+        {
+            Random random = new Random();
+
+            int[][] sofa = new int[3][];
+
+            int sizeTemp = 0;
+            int sizeMin = 1;
+            int sizeMax = 10;
+            for (int i = 0; i < sofa.Length; i++)
+            {
+                sizeMin += sizeTemp;
+                sizeMax = sizeMin + random.Next(1, 10);
+                sofa[i] = new int[random.Next(sizeMin, sizeMax)];
+                for (int j = 0; j < sofa[i].Length; j++)
+                {
+                    sofa[i][j] = random.Next(50, 300);
+                }
+                sizeTemp = sofa[i].Length;
+            }
+
+            Console.WriteLine("Зубчастий масив (sofa):");
+            for (int i = 0; i < sofa.Length; i++)
+            {
+                for (int j = 0; j < sofa[i].Length; j++)
+                {
+                    Console.Write(sofa[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < sofa.Length; i++)
+            {
+                Console.WriteLine($"Books in row {i}: {sofa[i].Length}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < sofa.Length; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < sofa[i].Length; j++)
+                {
+                    sum += sofa[i][j];
+                }
+                Console.WriteLine($"Sum of books in row {i}: {sum}");
+            }
+
+            Console.WriteLine();
+
+            int totalSum = 0;
+            for (int i = 0; i < sofa.Length; i++)
+            {
+                for (int j = 0; j < sofa[i].Length; j++)
+                {
+                    totalSum += sofa[i][j];
+                }
+            }
+            Console.WriteLine($"Total sum of books: {totalSum}");
+        }
+    }
+}
